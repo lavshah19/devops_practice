791d64696a04ec6c7072dc9b5f0b2bd7dbef64f0
lav kush <lavshah1111@gmail.com>
2025-10-16 14:49:01 +0545

    backen remen push

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const conntectTOMongoDB = require("./database/db");
+const taskRoute = require("./routes/task.route");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+conntectTOMongoDB();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.use("/api/tasks", taskRoute);
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+
+// 404 route handler
+app.use((req, res) => {
+  res.status(404).send("Page not found");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ backend/utils/CustomError.js
@@ -0,0 +1,10 @@
+class CustomError extends Error {
+  constructor(message, statusCode) {
+    super(message);          // call the parent constructor (Error)
+    this.statusCode = statusCode || 500; // default to 500 if not provided
+    this.name = this.constructor.name;   // sets the error name
+    Error.captureStackTrace(this, this.constructor); // optional, for cleaner stack
+  }
+}
+
+module.exports = CustomError;
